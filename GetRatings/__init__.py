456138ceb8da6b1c1b9b9d59ba879982ef221303
jmourgues44880 <114567902+jmourgues44880@users.noreply.github.com>
2022-09-28 15:37:10 +0200

    Update __init__.py

--- GetRatings/__init__.py
+++ GetRatings/__init__.py
@@ -1,24 +1,17 @@
 import logging
+import json
 
 import azure.functions as func
 
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
+def main(req: func.HttpRequest, documents: func.DocumentList) -> str:
     logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    if not documents:
+        logging.warning("ToDo item not found")
     else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        logging.info("Found ToDo item, Description=%s",
+                     documents[0]['timestamp'])
+    jsonString = ""
+    for document in documents:
+        jsonString += json.dumps(document.to_json())
+
+    return func.HttpResponse(f"{jsonString}")
